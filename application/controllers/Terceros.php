a609bca80e2f462c89acc5451fec6828c5867a17
SamuelCampo <samuelcampo18@gmail.com>
2020-06-02 14:27:24 -0500

    Update
    asd

---
+++ application/controllers/Terceros.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Terceros extends CI_Controller {
+
+	public function index()
+	{
+		echo "hola";		
+	}
+
+}
+
+/* End of file Terceros.php */
+/* Location: ./application/controllers/Terceros.php */
